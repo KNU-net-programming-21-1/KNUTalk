43ddf40d68278e8e4fe3f0c3b53236806431399e
sapiens2000 <cube9928@naver.com>
2021-06-05 17:16:19 +0900

    for test
    연결테스트 용으로 client 임시 작성

---
+++ Client/main.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <WinSock2.h>
+
+int main()
+{
+    WSADATA wsaData;
+    SOCKET hSocket;
+    SOCKADDR_IN servAdr;
+ 
+    int port;
+
+    if(WSAStartup(MAKEWORD(2, 2), &wsaData)!=0)
+    {
+        crit_error_handling("socket initialize failure");
+        exit(1);
+    }
+    
+    hSocket=socket(PF_INET, SOCK_STREAM, 0);
+    if(hSocket==INVALID_SOCKET)
+    {
+        crit_error_handling("socket() error.");
+    }    
+    
+    scanf("%d", &port);
+    memset(&servAdr, 0, sizeof(servAdr));
+	servAdr.sin_family = AF_INET;
+	servAdr.sin_addr.s_addr = htonl("127.0.0.1");                                   // 나중에 수정
+	servAdr.sin_port = htons(port);
+    
+    if(connect(hSocket, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR)
+        crit_error_handling("connect() error.");
+    else
+        puts("Connected......");
+
+    Sleep(500);
+
+    closesocket(hSocket);
+    WSACleanup();
+    return 0;
+}
