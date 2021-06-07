4f4f3717862dd388a25b9de19e7559b873ac1f31
한지예 <74859386+hanjeje20@users.noreply.github.com>
2021-06-07 18:26:32 +0900

    test clnt_main
    테스트용으로 사용하던 clnt의 main입니다. 로그인까지는 정상적으로 되던데 makeroom부터 오류가 나니 참고부탁드립니다.

--- Client/main.c
+++ Client/main.c
@@ -12,6 +12,7 @@ int main()
     char pw[PW_SIZE];
     int sendBytes, recvBytes;
     int port;
+	int menu;
 
     if(WSAStartup(MAKEWORD(2, 2), &wsaData)!=0)
     {
@@ -36,33 +37,209 @@ int main()
     else
         puts("Connected......");
 
-    /* Login test */
-    while(1){
-        printf("Login Test\n");
-        printf("ID : ");
-        scanf("%s",id);
-        printf("PW : ");
-        scanf("%s",pw);
-        
-        packet_login login;
-        login.size = sizeof(packet_login);
-        login.type = LOGIN;
-        strcpy(login.id, id);
-        strcpy(login.pw, pw);
+	/* test */
+	do
+	{
+		printf("\n*********************\n\n");
+		printf("QUIT < 0 >\t");
+		printf("REGISTER < 1 >\t\t");
+		printf("LOGIN < 2 >\n");
+		printf("\n*********************\n\n");
+		printf("menu : ");
+		scanf(" %d", &menu);
 
-        memcpy(packet, &login, login.size);
+		switch (menu)
+		{
+		case QUIT:
+			closesocket(hSocket);
+			exit(0);
+			break;
 
-        sendBytes = send(hSocket, packet, sizeof(packet_login), 0);
-        printf("Send data len : %d\n", sendBytes);
+		case REGISTER:
+		{
+			while (1)
+			{
+				packet_register CS_register;
+				CS_register.type = REGISTER;
+				CS_register.size = sizeof(packet_register);
 
+				printf("ID : ");
+				scanf(" %s", &CS_register.id);
+				printf("PW : ");
+				scanf(" %s", &CS_register.pw);
 
-        recvBytes = recv(hSocket, packet, sizeof(packet_accept), 0);
+				sendBytes = send(hSocket, (const char*)&CS_register, sizeof(packet_register), 0);
 
-        if (*(packet + 2) == TRUE) {
-            printf("Login Success!\n");
-            break;
-        }
-    }
+				recvBytes = recv(hSocket, packet, sizeof(packet_registered), 0);
+				packet_registered* SC_register = (packet_registered *)packet;
+
+				if (SC_register->accept == true)
+				{
+					printf("가입에 성공하였습니다.\n");
+					break;
+				}
+				else
+				{
+					printf("가입에 실패하였습니다. 다시 시도해주세요.\n");
+					break;
+				}
+			}
+			break;
+		} // case REGISTER end
+
+		case LOGIN:
+		{
+			while (1)
+			{
+				packet_login CS_login;
+				CS_login.type = LOGIN;
+				CS_login.size = sizeof(packet_login);
+
+				printf("ID : ");
+				scanf(" %s", &CS_login.id);
+				printf("PW : ");
+				scanf(" %s", &CS_login.pw);
+
+				send(hSocket, (const char*)&CS_login, sizeof(packet_login), 0);
+
+				recvBytes = recv(hSocket, packet, sizeof(packet_accept), 0);
+				packet_accept* SC_login = (packet_accept *)packet;
+
+				if (SC_login->accept == true)
+				{
+					printf("로그인에 성공하였습니다.\n");
+					break;
+				}
+				else
+				{
+					printf("로그인에 실패하였습니다. 다시 시도해주세요.\n");
+					break;
+				}
+			}
+			break;
+		} // case LOGIN end
+
+		default:
+			printf("잘못된 선택입니다. 다시 메뉴를 선택해주세요.\n");
+			break;
+		} // switch end
+	} while (menu != LOGIN);
+
+	/**********로그인 이후 *************/
+
+	do
+	{
+		printf("\n*********************\n\n");
+		printf("QUIT < 0 >\n");
+		printf("LOGOUT < 3 >\t");
+		printf("ENTER < 4 >\n");
+		printf("ROOMINFO < 8 >\t");
+		printf("MAKEROOM < 9 >\n");
+		printf("\n*********************\n\n");
+		printf("menu : ");
+		scanf(" %d", &menu);
+
+		switch (menu)
+		{
+		case QUIT:
+			closesocket(hSocket);
+			exit(0);
+			break;
+
+		case LOGOUT:
+		{
+			while (1) // 반복문?
+			{
+				packet_logout CS_logout;
+				CS_logout.type = REGISTER;
+				CS_logout.size = sizeof(packet_logout);
+
+				/*
+				printf("ID : ");
+				scanf(" %s", &CS_logout.id);
+				*/
+
+				sendBytes = send(hSocket, (const char*)&CS_logout, sizeof(packet_logout), 0);
+
+				recvBytes = recv(hSocket, packet, sizeof(packet_logout_accept), 0);
+				packet_logout_accept* SC_logout = (packet_logout_accept *)packet;
+
+				if (SC_logout->accept == true)
+				{
+					printf("로그아웃 됐습니다.\n");
+					break;
+				}
+				else
+				{
+					printf("로그아웃에 실패하였습니다. 다시 시도해주세요.\n");
+					break;
+				}
+			}
+			break;
+		} // case LOGOUT end
+
+		case ENTER:
+		{
+			packet_enter CS_enter;
+			CS_enter.type = LOGIN;
+			CS_enter.size = sizeof(packet_enter);
+
+			printf("room ID : ");
+			scanf(" %s", &CS_enter.room_id);
+
+			send(hSocket, (const char*)&CS_enter, sizeof(packet_enter), 0);
+
+			recvBytes = recv(hSocket, packet, sizeof(packet_join), 0);
+			packet_join* SC_enter = (packet_join *)packet;
+
+			if (SC_enter->accept == true)
+			{
+				printf("%d방에 입장하였습니다.\n", SC_enter->room_id);
+				break;
+			}
+			else
+			{
+				printf("%d방에 입장하지 못하였습니다.\n", SC_enter->room_id);
+				break;
+			}
+			break;
+		} // case ENTER end
+
+		case ROOMINFO:
+			break;
+
+		case MAKEROOM:	//수정필요
+		{
+			packet_makeroom CS_makeroom;
+			CS_makeroom.type = LOGIN;
+			CS_makeroom.size = sizeof(packet_makeroom);
+
+			printf("room ID : ");
+			scanf(" %s", &CS_makeroom.room_name);
+
+			send(hSocket, (const char*)&CS_makeroom, sizeof(packet_makeroom), 0);
+
+			recvBytes = recv(hSocket, packet, sizeof(packet_complete), 0);
+			packet_complete* SC_complete = (packet_complete *)packet;
+
+			if (SC_complete->accept == true)
+			{
+				printf("%d방을 생성하였습니다.\n", SC_complete->room_id);
+				break;
+			}
+			else
+			{
+				printf("%d방을 생성하지 못하였습니다.\n", SC_complete->room_id);
+				break;
+			}
+			break;
+		} // case MAKEROOM end
+
+		default:
+			printf("잘못된 선택입니다. 다시 메뉴를 선택해주세요.\n");
+			break;
+		} // switch end
+	} while (menu == MAKEROOM);
 
 
     closesocket(hSocket);
